d21b5121644ab228c141d9eef346714361303aba
saad rangara <rangarasaad@gmail.com>
2024-10-20 21:12:15 +0100

    nextconfig change due vercel erroe while deploying

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
+export default nextConfig;
